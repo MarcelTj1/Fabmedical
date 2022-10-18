ffa673bc40a7548fcbb6d7602f58cd0d8b4826c2
MarcelTj1 <marcel.tjia@adesso.de>
2022-10-18 13:17:29 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("9be22fd5-e031-48af-a2c4-2c8919ce9bb6");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
